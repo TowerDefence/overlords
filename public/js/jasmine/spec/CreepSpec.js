8b1556283f838a38de4b59ed2ffcac934d3164cc
kirstylm <apprentice@StMac2.local>
2015-04-23 15:15:45 +1200

    Add skeleton for Creep model

--- public/js/jasmine/spec/CreepSpec.js
+++ public/js/jasmine/spec/CreepSpec.js
@@ -10,19 +10,19 @@ describe("Creep", function() {
   it("should be able to move a creep", function() {
     var position = creep.position;
     creep.move();
-    expect(creep.position).toEqual(current_x + creep.speed);
+    expect(creep.position).toEqual(position + creep.speed);
   });
 
   it("should be able to get shot", function() {
     var lives = creep.lives;
     var damage = bullet.damage;
-    creep.get_shot(damage);
+    creep.getShot(damage);
     expect(creep.lives).toEqual(lives - damage);
   });
 
   it("should be able to die", function() {
     for (var k = 1; k <= creep.lives; k++ ) {
-      creep.get_shot();
+      creep.getShot();
     };
      expect(creep.alive).toEqual(false);
   });
---
+++ public/js/models/CreepModel.js
@@ -0,0 +1,21 @@
+
+function Creep(){
+  this.position = new Victor(0,0);
+  this.speed = new Victor(5,0);
+  this.height = 20;
+  this.width = 20;
+  this.alive = true;
+  this.lives = 3;
+}
+
+Creep.prototype.move = function() {
+
+}
+
+Creep.prototype.getShot = function() {
+
+}
+
+Creep.prototype.hasCollided = function() {
+
+}
