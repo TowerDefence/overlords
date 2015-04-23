a0751aedd62757a44d2ee904b539bc6ecb34645b
CaraHill & Piet Geursen <pair+CaraHill.pietgeursen@devbootcamp.com>
2015-04-23 16:25:34 +1200

    Pass choose closest target test

--- public/js/jasmine/spec/TowerSpec.js
+++ public/js/jasmine/spec/TowerSpec.js
@@ -14,17 +14,40 @@ describe("Tower", function() {
   describe("isInRange", function() {
 
     it("returns true if creep is in range ", function() {
-      expect(tower.isInRange(tower.position)).toEqual(true);
+      var inRangeTarget = {position: tower.position};
+      expect(tower.isInRange(inRangeTarget)).toEqual(true);
     });
 
     it("returns false if creep is not in range ", function() {
       var towerRange = tower.range;
       var unitVector = new Victor(5,5).norm();
       var outOfRangeVector = unitVector.multiply(new Victor(2 * towerRange,2 * towerRange));
+      var outOfRangeTarget = {position: outOfRangeVector};
 
-      expect(tower.isInRange(outOfRangeVector)).toEqual(false);
+      expect(tower.isInRange(outOfRangeTarget)).toEqual(false);
     });
   });
+
+  describe("chooseClosestTarget", function() {
+
+    it("stores the position of the closest target", function() {
+
+      var targets = [];
+      var closest = new Victor(1,1);
+      var mediumDistance = new Victor(2,2);
+      var furthest = new Victor(3,3);
+      targets.push({position: closest}, {position: mediumDistance}, {position: furthest});
+
+      tower.chooseClosestTarget(targets);
+      console.log(tower.closestTarget);
+
+      expect(tower.closestTarget.position.x).toEqual(closest.x);
+      expect(tower.closestTarget.position.y).toEqual(closest.y);
+    });
+
+
+  });
+
 
 
 
--- public/js/models/towerModel.js
+++ public/js/models/towerModel.js
@@ -1,12 +1,28 @@
 
 function Tower(position){
-  this.position = position;
+  this.position = position.clone();
   this.range = 10;
+  this.closestTarget = {position: new Victor(Infinity, Infinity)};
 }
 
 Tower.prototype = {
-  isInRange: function(targetPosition){
-    return targetPosition.subtract(this.position).length() < this.range;
+  isInRange: function(target){
+    return this.rangeToTarget(target)  < this.range;
+  },
+
+  rangeToTarget: function(target){
+    return target.position.subtract(this.position).length();
+  },
+
+  chooseClosestTarget: function(targets){
+    //go through all the targets. If it's distance is smaller than this.closestTarget, update this.closest target.
+    for(var i = 0; i < targets.length; i++) {
+      var target = targets[i];
+      if(this.rangeToTarget(target) < this.rangeToTarget(this.closestTarget)){
+        this.closestTarget = target;
+      }
+    }
+    return this.closestTarget;
   }
 
 
