4dc6129ec64b6a681a7c2eace187782de5fa3ba1
CaraHill & Piet Geursen <pair+CaraHill.pietgeursen@devbootcamp.com>
2015-04-23 15:25:25 +1200

    Pass range tests.

--- public/js/models/towerModel.js
+++ public/js/models/towerModel.js
@@ -1,5 +1,16 @@
 
-var Tower = function(position){
+function Tower(position){
   this.position = position;
+  this.range = 10;
+}
+
+Tower.prototype = {
+  isInRange: function(targetPosition){
+    return targetPosition.subtract(this.position).length() < this.range;
+  }
+
+
 
 }
+
+
