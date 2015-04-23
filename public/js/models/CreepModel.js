7712e2bb6535cf5290746df826d36b6d0958fea9
kirstylm <apprentice@StMac2.local>
2015-04-23 16:26:06 +1200

    Add protoype methods to creep model

--- public/js/models/CreepModel.js
+++ public/js/models/CreepModel.js
@@ -3,18 +3,32 @@ function Creep(){
   this.position = new Victor(0,0);
   this.speed = new Victor(5,0);
   this.radius = 10;
-  this.alive = true;
   this.lives = 3;
+  this.alive = true;
 }
 
 Creep.prototype.move = function() {
-
+  this.position = this.position.add(this.speed)
 }
 
-Creep.prototype.getShot = function() {
-
+Creep.prototype.getShot = function(damage) {
+  this.lives = this.lives - damage;
+  this.updateAlive();
 }
 
-Creep.prototype.hasCollided = function() {
+Creep.prototype.hasCollided = function(objectPos,objectRadius) {
+  var tempPosition = this.position;
+  var distance = tempPosition.subtract(objectRadius).length;
+  if ((distance - this.radius - objectRadius) <= 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
+Creep.prototype.updateAlive = function() {
+  if (this.lives = 0) {
+    this.alive = false;
+  }
 }
