a7a13634cf2649f6cebb19c9bd3b13efe06449dd
HarrietM <hsemckinnon@gmail.com>
2015-04-23 15:50:04 +1200

    Fix error. Clone vector

--- public/js/jasmine/src/BulletModel.js
+++ public/js/jasmine/src/BulletModel.js
@@ -2,12 +2,14 @@
 
 function Bullet(towerPosition, direction) {
   this.size = 1;
-  this.position = towerPosition;
-  this.direction = direction;
+  this.position = towerPosition.clone();
+  this.direction = direction.clone();
   this.damage = 1;
+  console.log(this.position);
 }
 
 
 Bullet.prototype.updatePosition = function() {
-  this.position = this.position.add(this.direction);
+  this.position.add(this.direction);
+  console.log(this.position);
 };
