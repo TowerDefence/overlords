3120603e85e0f3210a182a408a32fd8b33894a7e
CaraHill & Piet Geursen <pair+CaraHill.pietgeursen@devbootcamp.com>
2015-04-24 15:42:53 +1200

    Remove console logs.

--- public/js/models/towerModel.js
+++ public/js/models/towerModel.js
@@ -3,7 +3,7 @@ function TowerModel(position, id){
   this.id = id
   this.element = "<div class='tower' src=''> </div>"
   this.position = position.clone();
-  this.range = 10;
+  this.range = 500;
   this.radius = 5;
   this.fireRateMs = 1000;
   this.closestTarget = {position: new Victor(Infinity, Infinity)};
@@ -20,8 +20,7 @@ function TowerModel(position, id){
 
 TowerModel.prototype = {
   isInRange: function(target){
-    console.log("range to target:", this.rangeToTarget(target) )
-    return this.rangeToTarget(target)  < this.range;
+    return this.rangeToTarget(target) < this.range;
   },
 
   rangeToTarget: function(target){
@@ -34,18 +33,14 @@ TowerModel.prototype = {
       var target = targets[i];
       if(this.rangeToTarget(target) < this.rangeToTarget(this.closestTarget)){
         this.closestTarget = target;
-        console.log("Added new closest target: ", this.closestTarget);
       }
     }
-
   },
 
   fireAtTargetIfInRange: function(){
-    console.log("Aiming...");
-    if(this.isInRange(this.closestTarget)){
-      console.log("Firing. Stand back.", this.position);
-      return this.gun.shootBullet(this.position.clone(), this.closestTarget.position.clone());
 
+    if(this.isInRange(this.closestTarget)){
+      return this.gun.shootBullet(this.position, this.closestTarget.position);
     }
   },
 
